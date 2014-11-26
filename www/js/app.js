68b9170b642b287776804329dd59bc229cfcdb6f
Martín González <marto.gtk@gmail.com>
2014-11-26 15:13:38 -0300

    Mapa

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var example = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +16,47 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+example.controller('MapController', function($scope, $ionicLoading) {
+ 
+    google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(-33.4558,-70.6181);
+
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.HYBRID,
+            disableDefaultUI: true,
+            zoomControl: true
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        var image = '../img/tapa.png';
+        var newLatlng = new google.maps.LatLng(-33.4558,-70.6181);
+        var myLocation = new google.maps.Marker({
+                position: newLatlng,
+                icon: image,
+                map: map,
+                title: "My Location"
+        });
+        var myLocation2 = new google.maps.Marker({
+                position: new google.maps.LatLng(-33.4558,-70.6184),
+                icon: image,
+                map: map,
+                title: "My Location"
+        });
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            /*var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(-33.4558,-70.6181),
+                map: map,
+                title: "My Location"
+            });*/
+        });
+ 
+        $scope.map = map;
+    });
+ 
+});
