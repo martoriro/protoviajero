a2fc3242508756811bcfcf492fa35aa6d02a55ca
Martín González <marto.gtk@gmail.com>
2014-12-03 13:54:25 -0300

    Estructura fea de la aplicación

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform, $rootScope, $state) {
   $ionicPlatform.ready(function() {
@@ -16,6 +16,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
   });
+
+  $rootScope.$state = $state;
 
 })
 
@@ -42,12 +44,79 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     .state("app.login",{
       url:"/login",
       views:{
-        "menuContent" :{
+        'menuContent' :{
           templateUrl:"templates/login.html"
         }
       }
     })
 
+    .state("app.c",{
+      url:"/c",
+      views:{
+        'menuContent' :{
+          templateUrl:"templates/c.html"
+        }
+      }
+    })
+
+    .state('alojamiento', {
+      url: "/alojamiento",
+      abstract: true,
+      templateUrl: "templates/alojamiento.html",
+      controller: 'AlojamientoCtrl'
+    })
+
+    // Each tab has its own nav history stack:
+
+    .state('alojamiento.home', {
+      url: '/home',
+      views: {
+        'alojamiento-home': {
+          templateUrl: 'templates/alojamiento-home.html',
+          controller: 'AlojamientoCtrl'
+        }
+      }
+    })
+
+    .state('alojamiento.contacto',{
+      url: '/contacto',
+      views:{
+        'alojamiento-contacto':{
+          templateUrl: 'templates/alojamiento-contacto.html',
+          controller: 'ContactoCtrl'
+        }
+      }
+    })
+
+    .state('alojamiento.habitaciones', {
+      url: '/habitaciones',
+      views: {
+        'alojamiento-habitaciones': {
+          templateUrl: 'templates/alojamiento-habitaciones.html',
+          controller: 'HabitacionesCtrl'
+        }
+      }
+    })
+
+    .state('alojamiento.habitacion-detail', {
+      url: '/habitacion/:friendId',
+      views: {
+        'alojamiento-habitaciones': {
+          templateUrl: 'templates/habitacion-detail.html',
+          controller: 'FriendDetailCtrl'
+        }
+      }
+    })
+
+    .state('alojamiento.horarios', {
+      url: '/horarios',
+      views: {
+        'alojamiento-horarios': {
+          templateUrl: 'templates/alojamiento-horarios.html',
+          controller: 'HorariosCtrl'
+        }
+      }
+    });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,49 +6,119 @@ angular.module('starter.controllers', ['ionic'])
 .controller('HomeCtrl', function($scope){
 }) 
 
+.controller('AlojamientoCtrl', function($scope) {
+})
+
+/*.controller('ContactoCtrl', function($scope) {
+})*/
+
+.controller('HabitacionesCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('HorariosCtrl', function($scope) {
+})
+
 .controller("MapController", function($scope, $state){
+  var x2js = new X2JS();
+  console.log(x2js.xml2json('xml/lugares.xml'));
+
 	var MY_MAPTYPE_ID = 'custom_style';
 
 	var featureOpts = [{
-      stylers: [
-        { hue: '#ff8d00' },
-        { visibility: 'simplified' },
-        { gamma: 0.5 },
-        { weight: 0.5 }
-      ]}
-  	];
+    stylers: [
+      { hue: '#222222' },
+      { visibility: 'simplified' },
+      { gamma: 0.5 },
+      { weight: 0.5 }
+    ]}
+	];
 
 	var mapOptions = {
 		center: new google.maps.LatLng(-33.4500, -70.6667),
-          zoom: 8,
-          mapTypeControlOptions: {
-      		mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
-    		},
-    		mapTypeId: MY_MAPTYPE_ID,
-    		disableDefaultUI: true,
-    		zoomControl: true
+      zoom: 10,
+        mapTypeControlOptions: {
+    		mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+		},
+		mapTypeId: MY_MAPTYPE_ID,
+		disableDefaultUI: true,
+		zoomControl: true
 
-    };
+  };
 
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var styledMapOptions = {
-	    name: 'Viajero'
-  	};
+  var styledMapOptions = {
+    name: 'Viajero'
+	};
 
-  	var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+	var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
 
-  	var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(-33.4558, -70.6181),
-      map: map,
-      icon: 'img/botella.png',
-      title: 'Lugar 1'
-  	});
+  var imageL = {
+    url: 'img/lodging.png',
+    size: 15,
+    scaledSize: 10
+  };
 
-    google.maps.event.addListener(marker, 'click', function(){
-      $state.go('app.login');
-    });
+	var marker1 = new google.maps.Marker({
+    position: new google.maps.LatLng(-33.43663, -70.64445),
+    map: map,
+    icon:  new google.maps.MarkerImage(
+      'img/lodging.png',
+      new google.maps.Size(10, 15),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(0, 0),
+      new google.maps.Size(10, 15)
+    ),
+    title: 'Hostal y apartamentos Andes"'
+	});
+  google.maps.event.addListener(map, 'zoom_changed', function(){
+    var largo = 50 * (map.getZoom()-10);    
+    var ancho = largo / 1.42;
+    var icon = marker1.getIcon();
+    console.log(icon);
+    marker1.setIcon(new google.maps.MarkerImage(
+            icon.url,
+            new google.maps.Size(largo,  ancho),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(0, 0),
+            new google.maps.Size(largo, ancho)
+        ));
+    console.log(icon);  
+  });
 
-	map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+  var imageD = {
+    url: 'img/diner.png',    
+    scale: 10
+  }
+
+  var marker2 = new google.maps.Marker({
+    position: new google.maps.LatLng(-33.43733, -70.64186),
+    map: map,
+    icon: imageD,
+    title: 'Bombón oriental"'
+  });
+
+  var imageA = {
+    url: 'img/adventure.png',    
+    scale: 10
+  }
+
+  var marker3 = new google.maps.Marker({
+    position: new google.maps.LatLng(-33.58393, -70.39690),
+    map: map,
+    icon: imageA,
+    title: 'Ruta vertical"'
+  });
+
+  google.maps.event.addListener(marker1, 'click', function(){
+    $state.go('alojamiento.home');
+  });
+
+	map.mapTypes.set(MY_MAPTYPE_ID, customMapType);  
 
 });
