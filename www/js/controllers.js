e55d2e556a3167eb6dafeb30559647ef9d778363
Martín González <marto.gtk@gmail.com>
2014-12-04 21:05:25 -0300

    Geolocalización 1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -72,6 +72,15 @@ angular.module('starter.controllers', ['ionic'])
 
   };
 
+  navigator.geolocation.getCurrentPosition(function(pos) {
+      map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      var myLocation = new google.maps.Marker({
+          position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+          map: map,
+          title: "Aquí"
+      });
+  });
+
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
   var styledMapOptions = {
